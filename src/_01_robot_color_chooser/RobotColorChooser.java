2c7e7ad4fcfd7a2e011beb07c8adec07247cba8b
curry.zeke <curry.zeke@SEMS-259-17.SEMS259.LAB>
2020-02-26 16:24:51 -0800

    robotColor

--- src/_01_robot_color_chooser/RobotColorChooser.java
+++ src/_01_robot_color_chooser/RobotColorChooser.java
@@ -4,23 +4,51 @@
 package _01_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		//1. Create a new Robot
-		
+		Robot bot = new Robot();
 		//3. Ask the user what color they would like the robot to draw
+	for (int i = 0; i < 100; i++) {
 		
+	
+		String color =	JOptionPane.showInputDialog("what color would you like the square to be");
 		//5. Use an if/else statement to set the pen color that the user requested
-
+if (color.contentEquals("red")) {
+	bot.setPenColor(Color.red);
+} else {
+bot.setRandomPenColor();
+}
         //6. If the user doesn’t enter anything, choose a random color
 
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
+
+			
 		
 		//4. Set the pen width to 10
-		
+		bot.setPenWidth(10);
 	    //2. Make the robot draw a shape (this will take more than one line of code)
+bot.penDown();
+bot.setSpeed(100);
+
+bot.move(100);	
+bot.turn(90);
+
+
+}
+		
+
+
+
+
+
+
+
 
 
 	}
--- src/_02_crazy_cat_lady/CrazyCatLady.java
+++ src/_02_crazy_cat_lady/CrazyCatLady.java
@@ -5,14 +5,20 @@ package _02_crazy_cat_lady;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
+String cat = JOptionPane.showInputDialog("how many cats do you have");
 		// 2. Convert their answer into an int
-
+int CAT= Integer.parseInt(cat);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
+if (if CAT) {
+	
+} else {
 
+}
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
 
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
